70aadee9b80c10cb4beb8f7b26d7725c8e0a10f0
namchid <ndo@trinity.edu>
2016-10-03 14:53:25 -0700

    final (?) cleanup for prev commit

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ var listening = false
 var gRes = null, gReq = null
 
 // Setup for Electron app
-
 function createWindow() {
 	win = new BrowserWindow({width: 800, height: 600})
 	win.loadURL(`file://${__dirname}/_server_index.html`)
@@ -51,13 +50,10 @@ app.on('activate', function () {
 
 // IPC functions
 ipcMain.on('receiveSerializedDOM', (_, contents) => {
-    // var serialize = require('dom-serialize')
-    console.log("Ending req's: " + gReq.url)
-	gRes.end(contents)
+	gRes.end('<html>' + contents + '</html>')
 })
 
 expressApp.get('/index\.html', (req, res) => {
-    console.log("Received req: " + req.url)
     win.loadURL('file://' + __dirname + req.url)
     gRes = res
     gReq = req
@@ -71,10 +67,7 @@ expressApp.get('*', (req, res) => {
     var parsed_url = url.parse(req.url, true)
     var filename = parsed_url.pathname.substr(1)
 
-
     var p = path.join(__dirname, filename)
-    console.log("pathpathpath")
-    console.log(p)
     res.sendFile(p)
 })
 
