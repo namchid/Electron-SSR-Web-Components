74cfd04dbae357eb5123807bceee0fbb6a99f77f
namchid <ndo@trinity.edu>
2016-10-25 14:18:14 -0700

    Realized there was a problem with the way attributes were being serialized. This should fix it. Rewriting to fix other issues too.

--- serialize.js
+++ serialize.js
@@ -1,39 +1,84 @@
+var htmlImports = [];
+
 function recurseParseDOM(node) {
-    if(node == null) return '';
+    if (node == null) return '';
 
-    var outerHTML = '';
+    var tag = node.tagName;
+    var openTag = '';
+    var closeTag = '';
 
-    if(node.outerHTML) {
-        var outerHTML = node.outerHTML.split('>');
-        console.log(outerHTML + '>');
+    var htmlString = '';
+
+    if(tag) {
+        tag = tag.toLowerCase();
+        var attributes = [];
+        var values = [];
+
+        for(var i = 0; i < node.attributes.length; i++) {
+            var curr = node.attributes[i];
+            attributes.push(curr.nodeName);
+            values.push(curr.nodeValue);
+        }
+
+        var attributeString = ' ';
+        for(var i = 0; i < attributes.length; i++) {
+            attributeString += attributes[i] + '="' + values[i] + '" ';
+        }
+
+        openTag = '<' + tag + attributeString + '>';
+        closeTag = '</' + tag + '>';
     }
 
-    // this is the "innerHTML"
-    if(node.childNodes) {
-        parseChildNodes(node.childNodes);
-    } 
-    if(node.shadowRoot) {
-        parseChildNodes(node.shadowRoot.childNodes);
-    }
+    htmlString += openTag;
+    htmlString += closeTag;
 
-    if(outerHTML) {
-        var tag = node.tagName;
-        console.log('</' + tag.toLowerCase() + '>');
-    }
+    return htmlString;
 }
 
-function parseChildNodes(nodes) {
+// function recurseParseDOM(node) {
+//     if(node == null) return '';
 
-    for(var i = 0; i < nodes.length; i++) {
-        if(nodes[i].innerHTML) {
-            console.log(nodes[i].innerHTML);
-        }
-        if(nodes[i].shadowRoot) {
-            recurseParseDOM(nodes[i]);
-        } else if(nodes[i].childNodes) {
-            recurseParseDOM(nodes[i]);
-        }
-    }
-}
+//     var outerHTML = '';
 
-recurseParseDOM(document.documentElement);
+//     if(node.outerHTML) {
+//         var outerHTML = node.outerHTML.split('>');
+//         console.log(outerHTML[0] + '>');
+//     }
+
+//     // var tag = node.tagName;
+
+//     if(tag) {
+//         var atts = [];
+
+//         console.log('<' + tag + '>');
+//     }
+
+//     // this is the "innerHTML"
+//     if(node.childNodes) {
+//         parseChildNodes(node.childNodes);
+//     } 
+//     if(node.shadowRoot) {
+//         parseChildNodes(node.shadowRoot.childNodes);
+//     }
+
+//     if(outerHTML) {
+//         var tag = node.tagName;
+//         console.log('</' + tag.toLowerCase() + '>');
+//     }
+// }
+
+// function parseChildNodes(nodes) {
+
+//     for(var i = 0; i < nodes.length; i++) {
+//         if(nodes[i].innerHTML) {
+//             console.log(nodes[i].innerHTML);
+//         }
+//         if(nodes[i].shadowRoot) {
+//             recurseParseDOM(nodes[i]);
+//         } else if(nodes[i].childNodes) {
+//             recurseParseDOM(nodes[i]);
+//         }
+//     }
+// }
+
+// recurseParseDOM(document.documentElement);
