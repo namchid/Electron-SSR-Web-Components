a9008c457c2f49f6e07ee0f22b05e18838736ed5
namchid <ndo@trinity.edu>
2016-10-25 13:34:05 -0700

    fixed problem of tag attributes not being serialized

--- serialize.js
+++ serialize.js
@@ -1,10 +1,11 @@
-
 function recurseParseDOM(node) {
     if(node == null) return '';
 
-    var tag = node.tagName;
-    if(tag) {
-        console.log('<' + tag + '>');
+    var outerHTML = '';
+
+    if(node.outerHTML) {
+        var outerHTML = node.outerHTML.split('>');
+        console.log(outerHTML + '>');
     }
 
     // this is the "innerHTML"
@@ -14,12 +15,10 @@ function recurseParseDOM(node) {
     if(node.shadowRoot) {
         parseChildNodes(node.shadowRoot.childNodes);
     }
-    // if(node.innerHTML) {
-    //     console.log(node.innerHTML);
-    // }
 
-    if(tag) {
-        console.log('</' + tag + '>');
+    if(outerHTML) {
+        var tag = node.tagName;
+        console.log('</' + tag.toLowerCase() + '>');
     }
 }
 
@@ -29,55 +28,10 @@ function parseChildNodes(nodes) {
         if(nodes[i].innerHTML) {
             console.log(nodes[i].innerHTML);
         }
-
         if(nodes[i].shadowRoot) {
             recurseParseDOM(nodes[i]);
-            //console.log(nodes[i])
-            //console.log(nodes[i].shadowRoot.childNodes)
-            //recurseParseDOM(nodes[i].shadowRoot.childNodes);
         } else if(nodes[i].childNodes) {
             recurseParseDOM(nodes[i]);
-        } 
-
-
-
-        // else {
-        //     var tag = nodes[i].tagName;
-        //     if(tag) {
-        //         console.log('<' + tag + '>');
-        //         console.log(nodes[i].innerHTML);
-        //         console.log('</' + tag + '>');
-        //     } else {
-        //         console.log('.....text.....')
-        //     }
-        // }
+        }
     }
 }
-
-
-// var domString = '';
-// var cloned = document.cloneNode(true);
-// var stack = [];
-
-// function serialize(node) {
-
-//     stack = [].slice.call(document.querySelectorAll('*'));
-//     replaceAndSerialize();
-
-// }
-
-// function replaceAndSerialize() {
-//     if(stack.length < 2) return;
-
-// }
-
-
-// function replaceAndSerializeHelper() {
-//     var top = stack.pop();
-
-//     var temp = [].slice.call(top.querySelectorAll('*'));
-
-
-// }
-
-// serialize(cloned);
