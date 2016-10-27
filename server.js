c62113fc7a129a58194afe914297963d1d8bbc64
namchid <ndo@trinity.edu>
2016-10-27 09:36:05 -0700

    added the shadow dom implementation to the server

--- server.js
+++ server.js
@@ -104,27 +104,105 @@ expressApp.on('listening', () => {
 function getDOMInsidePage() {
     win.webContents.executeJavaScript(`
         var ipc = require('electron').ipcRenderer;
-        var serializer = require('dom-serialize');
-        var htmlImports = document.querySelectorAll('link[rel="import"]');
 
-        if(htmlImports.length > 1) {
-            var html = document.cloneNode(true);
-            var asyncFile = 'asyncFile.html'
-            var asyncImports = serializer(htmlImports);
-            htmlImports = html.querySelectorAll('link[rel="import"]');
+        var htmlImportsString = '';
+        var htmlImportsSet = false;
 
+        function handleHTMLImports() {
+            htmlImportsString = '';
+
+            var htmlImports = document.querySelectorAll('link[rel="import"]');
             for(var i = 0; i < htmlImports.length; i++) {
-                htmlImports[i].parentNode.removeChild(htmlImports[i]);
+                htmlImportsString += '<link ' + getAttributeString(htmlImports[i]).slice(0, -1) + '></link>';
             }
-            ipc.send('setAsyncImports', asyncImports);
-            html.querySelector('head').innerHTML += 
-                '<link rel="import" href="' + asyncFile + '" async></link>';
-            ipc.send('receiveSerializedDOM', html.documentElement.outerHTML);
-        } else if(htmlImports.length > 0) {
-            htmlImports[0].setAttribute('async', '');
-            ipc.send('receiveSerializedDOM', document.documentElement.outerHTML);
-        } else {
-            ipc.send('receiveSerializedDOM', document.documentElement.outerHTML);
         }
-    `)
+
+        function getAttributeString(node) {
+            var attributeString = '';
+
+            var attributes = [];
+            var values = [];
+
+            for(var i = 0; i < node.attributes.length; i++) {
+                var curr = node.attributes[i];
+                attributes.push(curr.nodeName);
+                values.push(curr.nodeValue);
+            }
+
+            for(var i = 0; i < attributes.length; i++) {
+                attributeString += attributes[i] + '="' + values[i] + '" ';
+            }
+
+            return attributeString;
+        }
+
+        function recurseParseDOM(node) {
+            if (node == null) return '';
+
+            var tag = node.tagName;
+            var openTag = '';
+            var closeTag = '';
+
+            var htmlString = '';
+
+            if(tag) {
+                tag = tag.toLowerCase();
+
+                var attributeString = ' ';
+
+                // don't serialize this
+                if(tag === 'link' && node.getAttribute('rel') === 'import') {
+                    if(!htmlImportsSet) {
+                        openTag = '<link rel="import" href="asyncFile.html" async>';
+                        closeTag = '</link>';
+                        htmlImportsSet = true;
+                    }
+                } else {
+                    attributeString += + getAttributeString(node);
+                    openTag = '<' + tag + attributeString.slice(0, -1) + '>';
+                    closeTag = '</' + tag + '>';
+                }
+            }
+
+            htmlString += openTag;
+
+            // get inner html
+            if(node.childNodes) {
+                htmlString += parseChildNodes(node.childNodes);
+            }
+            if(node.shadowRoot) {
+                htmlString += parseChildNodes(node.shadowRoot.childNodes);
+            }
+
+            htmlString += closeTag;
+
+            return htmlString;
+        }
+
+        function parseChildNodes(nodes) {
+
+            var htmlString = '';
+
+            for(var i = 0; i < nodes.length; i++) {
+                var nodeType = nodes[i].nodeType;
+
+                if(nodeType == 3) {
+                    htmlString += nodes[i].nodeValue;
+                } else if(nodeType == 8) {
+                    htmlString += '<!--' + nodes[i].nodeValue + '-->';
+                } else {
+                    if(nodes[i].childNodes || nodes[i].shadowRoot) {
+                        htmlString += recurseParseDOM(nodes[i]);
+                    }
+                }
+            }
+
+            return htmlString;
+        }
+
+        handleHTMLImports();
+        ipc.send('setAsyncImports', htmlImportsString);
+
+        ipc.send('receiveSerializedDOM', recurseParseDOM(document.documentElement));
+    `);
 }
