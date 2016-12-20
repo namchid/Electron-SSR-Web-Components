d2fac245415f4b24e2787ab0428a7a57252703b2
namchid <ndo@trinity.edu>
2016-12-19 16:30:14 -0800

    Refactored hybrid and shady versions a bit by moving similar code to other functions. Renamed some variables in shadow version to standardize names. More refactoring may come later. Change corresponds to PR#24.

--- _server_execute_js.js
+++ _server_execute_js.js
@@ -2,52 +2,19 @@ module.exports = function() {
   return {
     hybridVersion: function() {
       const ipcRenderer = require('electron').ipcRenderer;
-      const remote = require('electron').remote;
-      const directory = remote.getGlobal('directory');
-      const hash = require('string-hash');
-      let asyncImports = '';
 
-      let htmlImports = document.querySelectorAll('link[rel="import"]');
-
-      if (htmlImports.length > 0) {
+      if (document.querySelectorAll('link[rel="import"]').length > 0) {
         let html = document.cloneNode(true);
         html.querySelector('body').removeAttribute('unresolved');
 
-        let imports = html.querySelectorAll('link[rel="import"]');
-        const head = html.querySelector('head');
         let shadowPolymerScript = document.createElement('script');
         shadowPolymerScript.innerText =
             'window.Polymer = { dom: "shadow", lazyRegister: true}';
-        head.insertBefore(shadowPolymerScript, imports[0]);
+        let imports = html.querySelectorAll('link[rel="import"]');
+        html.querySelector('head').insertBefore(shadowPolymerScript, imports[0]);
 
-        let hashString = '';
-        imports.forEach((linkNode) => {
-          asyncImports += linkNode.outerHTML;
-          hashString += linkNode['href'];
-          linkNode.parentNode.removeChild(linkNode);
-        });
-        const hashed = hash(hashString);
-        ipcRenderer.send('setAsyncImports', hashed, asyncImports);
-
-        let newImport = html.createElement('link');
-        newImport.setAttribute('rel', 'import');
-        newImport.setAttribute('href', '_asyncImport' + hashed + '.html');
-        newImport.setAttribute('async', '');
-        html.querySelector('head').appendChild(newImport);
-
-        // Make sure pathnames are relative
-        html.querySelectorAll('[url]').forEach(
-            (link) => {
-                link.setAttribute('url', link.url.replace(directory, ''));
-              });
-        html.querySelectorAll('[src]').forEach(
-            (link) => {
-                link.setAttribute('src', link.src.replace(directory, ''));
-              });
-        html.querySelectorAll('[href]').forEach(
-            (link) => {
-              link.setAttribute('href', link.href.replace(directory, ''));
-            });
+        removeImportsAndSetInCache(html);
+        makePathsRelative(html);
 
         ipcRenderer.send('receiveSerializedDOM',
                          html.documentElement.outerHTML, false);
@@ -58,50 +25,12 @@ module.exports = function() {
     },
     shadyVersion: function() {
       const ipcRenderer = require('electron').ipcRenderer;
-      const remote = require('electron').remote;
-      const directory = remote.getGlobal('directory');
-      const hash = require('string-hash');
-      let asyncImports = '';
 
-      let htmlImports = document.querySelectorAll('link[rel="import"]');
-
-      if (htmlImports.length > 0) {
+      if (document.querySelectorAll('link[rel="import"]').length > 0) {
         let html = document.cloneNode(true);
         html.querySelector('body').removeAttribute('unresolved');
-
-        let hashString = '';
-        let imports = html.querySelectorAll('link[rel="import"]');
-        imports.forEach((linkNode) => {
-          asyncImports += linkNode.outerHTML;
-          hashString += linkNode['href'];
-          linkNode.parentNode.removeChild(linkNode);
-        });
-        const hashed = hash(hashString);
-        ipcRenderer.sendSync('setAsyncImports', hashed, asyncImports);
-
-        let newImport = html.createElement('link');
-        newImport.setAttribute('rel', 'import');
-        newImport.setAttribute('href',
-                               '_asyncImport' + hashed + '.html');
-        newImport.setAttribute('async', '');
-        html.querySelector('head').appendChild(newImport);
-
-        // Make sure pathnames are relative
-        html.querySelectorAll('[url]').forEach(
-            (link) => {
-                link.setAttribute('url',
-                  link.url.replace(directory, ''));
-              });
-        html.querySelectorAll('[src]').forEach(
-            (link) => {
-                link.setAttribute('src',
-                  link.src.replace(directory, ''));
-              });
-        html.querySelectorAll('[href]').forEach(
-            (link) => {
-              link.setAttribute('href',
-                link.href.replace(directory, ''));
-            });
+        removeImportsAndSetInCache(html);
+        makePathsRelative(html);
 
         ipcRenderer.send('receiveSerializedDOM',
                          html.documentElement.outerHTML, true);
@@ -113,12 +42,10 @@ module.exports = function() {
     /** Modified from Kevin's WC-SSR (link in README)**/
     shadowVersion: function() {
       const ipcRenderer = require('electron').ipcRenderer;
-      const remote = require('electron').remote;
-      const directory = remote.getGlobal('directory');
       const hash = require('string-hash');
-      let hashString = '';
+      let hashKey = '';
 
-      let asyncImports = '';
+      let hashValue = '';
       let removedImports = new Set();
 
       let shadowStyleList = [];
@@ -144,8 +71,8 @@ module.exports = function() {
         [].slice.call(imports).forEach(function(el) {
           const temp = el.outerHTML;
           if (!removedImports.has(temp)) {
-            hashString += el['href'];
-            asyncImports += el.outerHTML + String.fromCharCode(13);
+            hashKey += el['href'];
+            hashValue += el.outerHTML + String.fromCharCode(13);
             removedImports.add(temp);
           }
           el.remove();
@@ -270,13 +197,13 @@ module.exports = function() {
 
       /**
       * Inserts route for asynchronously requesting the HTML imports.
-      * @param {String} hashedURL The hashed key corresponding to the HTML
+      * @param {String} hashedNum The hashed key corresponding to the HTML
       * imports string in the server's cache.
       */
-      function insertImportLink(hashedURL) {
+      function insertImportLink(hashedNum) {
         let linkNode = document.createElement('link');
         linkNode.setAttribute('rel', 'import');
-        linkNode.setAttribute('href', hashedURL);
+        linkNode.setAttribute('href', hashedNum);
         linkNode.setAttribute('async', '');
         const head = document.querySelector('head');
         head.appendChild(linkNode);
@@ -285,13 +212,13 @@ module.exports = function() {
       /**
       * Registers custom elements, shadow roots, and reinserts link to
       * import HTML.
-      * @param {String} importURL The hashed key corresponding to the HTML
+      * @param {String} hashedNum The hashed key corresponding to the HTML
       * imports string in the server's cache.
       */
-      function registerAndReinsert(importURL) {
+      function registerAndReinsert(hashedNum) {
         insertPolymerShadowDom();
         registerShadowRoot();
-        insertImportLink(importURL);
+        insertImportLink(hashedNum);
       }
 
       /**
@@ -322,29 +249,55 @@ module.exports = function() {
 
       replaceShadowRoots(doc, clonedDoc);
       removeImports(clonedDoc);
-      let hashed = hash(hashString);
-      if (asyncImports != '') {
-        ipcRenderer.send('setAsyncImports', hashed, asyncImports);
+      hashKey = hash(hashKey);
+      if(hashValue != '') {
+        ipcRenderer.send('setAsyncImports', hashKey, hashValue);
       }
       removeScripts(clonedDoc);
       insertShadowStyles(clonedDoc, shadowStyleList);
-      insertScriptsAndImports(clonedDoc, hashed);
+      insertScriptsAndImports(clonedDoc, hashKey);
 
-      // Make sure pathnames are relative
-      clonedDoc.querySelectorAll('[url]').forEach(
-          (link) => {
-              link.setAttribute('url', link.url.replace(directory, ''));
-            });
-      clonedDoc.querySelectorAll('[src]').forEach(
-          (link) => {
-              link.setAttribute('src', link.src.replace(directory, ''));
-            });
-      clonedDoc.querySelectorAll('[href]').forEach(
-          (link) => {
-              link.setAttribute('href', link.href.replace(directory, ''));
-            });
+      makePathsRelative(clonedDoc);
 
       ipcRenderer.send('receiveSerializedDOM', clonedDoc.outerHTML, false);
     },
   };
 };
+
+function makePathsRelative(container) {
+  const remote = require('electron').remote;
+  const directory = remote.getGlobal('directory');
+  container.querySelectorAll('[url]').forEach(
+      (link) => {
+          link.setAttribute('url', link.url.replace(directory, ''));
+        });
+  container.querySelectorAll('[src]').forEach(
+      (link) => {
+          link.setAttribute('src', link.src.replace(directory, ''));
+        });
+  container.querySelectorAll('[href]').forEach(
+      (link) => {
+          link.setAttribute('href', link.href.replace(directory, ''));
+        });
+};
+
+function removeImportsAndSetInCache(container) {
+  const hash = require('string-hash');
+  let hashKey = '';
+  let hashValue = '';
+
+  let imports = container.querySelectorAll('link[rel="import"]');
+  imports.forEach((linkNode) => {
+    hashValue += linkNode.outerHTML;
+    hashKey += linkNode['href'];
+      linkNode.parentNode.removeChild(linkNode);
+    });
+  hashKey = hash(hashKey);
+  require('electron').ipcRenderer.sendSync('setAsyncImports', hashKey, hashValue);
+
+  let newImport = container.createElement('link');
+  newImport.setAttribute('rel', 'import');
+  newImport.setAttribute('href', '_asyncImport' + hashKey + '.html');
+  newImport.setAttribute('async', '');
+  container.querySelector('head').appendChild(newImport);
+}
